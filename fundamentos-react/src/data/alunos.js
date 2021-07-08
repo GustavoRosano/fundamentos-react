0669dd4bda29c5ba35ca52f796aa30b613fe9859
Gustavo Rosano Teixeira <gurosano@gmail.com>
2021-07-08 20:46:35 -0300

    adicionei a pasta data

---
+++ fundamentos-react/src/data/alunos.js
@@ -0,0 +1,11 @@
+export default [
+    { id: 1, nome:'Gustavo', nota: 9.2},
+    { id: 2, nome:'Claudio', nota: 8.5},
+    { id: 3, nome:'Pedro', nota: 5.2},
+    { id: 4, nome:'Marcos', nota: 7.0},
+    { id: 5, nome:'Rafael', nota: 8.3},
+    { id: 6, nome:'Vitor', nota: 9.8},
+    { id: 7, nome:'Paulo', nota: 10.0},
+    { id: 8, nome:'João', nota: 7.3},
+    { id: 9, nome:'Augusto', nota: 3.5}
+]
---
+++ fundamentos-react/src/data/produtos.js
@@ -0,0 +1,14 @@
+const produtos = [
+    { id: 1,   nome: 'Play Station 4',  preco: 3999.99 },
+    { id: 2,   nome: 'Smartwatch',      preco: 79.99 },
+    { id: 3,   nome: 'Iphone 11',       preco: 4499.99 },
+    { id: 4,   nome: 'Tv Sansung',      preco: 2999.99 },
+    { id: 5,   nome: 'Impressora',      preco: 999.99 },
+    { id: 6,   nome: 'Redmi Airdots',   preco: 189.99 },
+    { id: 7,   nome: 'Teclado Razer',   preco: 1649.99 },
+    { id: 8,   nome: 'Controle PS4',    preco: 199.99 },
+    { id: 9,   nome: 'Fone',            preco: 45.99 },
+    { id: 10,  nome: 'Carregador',      preco: 49.99 }
+]
+
+export default produtos 
